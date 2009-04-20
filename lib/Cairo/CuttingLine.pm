cd6d55c3f896e2a16d1ca3f1185b7d72ef3948fd
c9s <cornelius.howl@gmail.com>
2009-04-20 17:17:01 +0800

    add  test

--- lib/Cairo/CuttingLine.pm
+++ lib/Cairo/CuttingLine.pm
@@ -38,6 +38,72 @@ if you don't export anything, such as for a purely object-oriented module.
 
 =head1 FUNCTIONS
 
+=cut
+
+sub new {
+    my $class = shift;
+    my $self = {};
+    bless $self , $class;
+    $self->{cr} = shift;
+    return $self;
+}
+
+sub length {
+    my $self = shift;
+    $self->{length} = shift if @_;
+    $self->{length};
+}
+
+sub size {
+    my $self = shift;
+    $self->{size} = { @_ } if @_;
+    $self->{size};
+}
+
+sub stroke {
+    my $self = shift;
+#    my ( $abbr, $page_cr, $h, $v ) = @_;
+#    $page_cr->save;
+#
+#    my $config = _get_config $abbr;
+#
+#    my $p_x = ( $config->{h_margin} * 2 + $config->{canvas_w} ) * ($h) 
+#                + $config->{h_margin};
+#    my $p_y = ( $config->{v_margin} * 2 + $config->{canvas_h} ) * ($v) 
+#                + $config->{v_margin};
+#
+#    $page_cr->set_source_rgba( @line_rgba );
+#    $page_cr->set_line_width( $line_width );
+#    for my $p ( 0 .. 3 ) {
+#        my ($c_x,$c_y) = ($p_x,$p_y);
+#        if( $p & 1 ) {
+#            $c_x += $config->{canvas_w};
+#        }
+#        if( $p & 2 ) {
+#            $c_y += $config->{canvas_h};
+#        }
+#
+#        $page_cr->move_to( $c_x , $c_y );
+#        $page_cr->line_to(
+#            $c_x + ( $p & 1 ? $line_len : -$line_len ),
+#            $c_y
+#        );
+#
+#        $page_cr->move_to( $c_x , $c_y );
+#        $page_cr->line_to(
+#            $c_x,
+#            $c_y + ( $p & 2 ? $line_len : -$line_len ),
+#        );
+#        $page_cr->stroke();
+#    }
+#    $page_cr->show_page;
+#    $page_cr->restore;
+}
+
+
+
+
+
 
 
 
--- t/00-load.t
+++ t/00-load.t
@@ -1,9 +1,22 @@
-#!perl -T
-
-use Test::More tests => 1;
+#!perl
+use lib 'lib';
+use Test::More tests => 4;
 
 BEGIN {
 	use_ok( 'Cairo::CuttingLine' );
 }
 
 diag( "Testing Cairo::CuttingLine $Cairo::CuttingLine::VERSION, Perl $], $^X" );
+
+use Cairo;
+use Cairo::CuttingLine;
+
+my $cr = { };
+my $page = Cairo::CuttingLine->new( $cr );
+$page->size( width => 100 , height => 100 );
+my $size = $page->size;
+is( $size->{width} , 100 );
+is( $size->{height} , 100 );
+
+$page->length( 10 );
+is( $page->length , 10 );
