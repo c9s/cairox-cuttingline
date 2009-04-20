d94c771df1122e37b6cda4fe1f96ac1f0834c194
c9s <cornelius.howl@gmail.com>
2009-04-20 18:03:36 +0800

    fix test

--- lib/Cairo/CuttingLine.pm
+++ lib/Cairo/CuttingLine.pm
@@ -9,11 +9,11 @@ Cairo::CuttingLine - draw cutting line to cairo surface
 
 =head1 VERSION
 
-Version 0.02
+Version 0.03
 
 =cut
 
-our $VERSION = '0.02';
+our $VERSION = '0.03';
 
 
 =head1 SYNOPSIS
