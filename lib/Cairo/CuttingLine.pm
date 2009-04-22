28d8b4355e04aa17fffbdf8b5c7f6805dd17d00d
c9s <cornelius.howl@gmail.com>
2009-04-23 00:09:09 +0800

    bump version

--- lib/Cairo/CuttingLine.pm
+++ lib/Cairo/CuttingLine.pm
@@ -9,11 +9,11 @@ Cairo::CuttingLine - draw cutting line to cairo surface
 
 =head1 VERSION
 
-Version 0.03
+Version 0.04
 
 =cut
 
-our $VERSION = '0.03';
+our $VERSION = '0.04';
 
 
 =head1 SYNOPSIS
